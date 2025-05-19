60a2e85a2d4b64029538bdb8f96ab8420bb4eafc
yasirldn <119271926+yasirldn@users.noreply.github.com>
2025-05-19 14:53:17 +0100

    Update firebase_config.py
    Using streamlit secrets

--- firebase_config.py
+++ firebase_config.py
@@ -2,8 +2,8 @@ import firebase_admin
 from firebase_admin import credentials, auth, db, storage
 import streamlit as st
 
-cred = credentials.Certificate("~/.firebase/serviceAccountKey.json")
-# cred = credentials.Certificate(st.secrets["firebase"])
+# cred = credentials.Certificate("~/.firebase/serviceAccountKey.json")
+cred = credentials.Certificate(st.secrets["firebase"])
 
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://ai-for-new-students-default-rtdb.europe-west1.firebasedatabase.app',  # This is my database URL
